b626dfa07c7adf1e6910540095969bc900ae9760
Henrikas <gyngga@gmail.com>
2015-05-10 16:54:27 +0300

    Progressed on info exchange algorithm

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 	var Node = function(block) {
         this.blockInstance = block;
         this.name = nodes.length;
-        this.table = [{destination: this.name, nextHop: 0, hopCount: 0, seqNo: 0}];
+        this.table = [{destination: this.name, nextHop: this.name, hopCount: 0, seqNo: 0}];
 
         nodeInfoContainer = document.createElement('div');
 		nodeInfoContainer.className = 'node-info-container';
@@ -41,21 +41,21 @@ $(document).ready(function() {
         destinationInfo = document.createElement('div');
         destinationInfo.className = 'destination-info';
         columnTitle = document.createElement('p');
-        columnTitle.innerHTML = 'Destination';
+        columnTitle.innerHTML = 'Dest.';
         destinationInfo.appendChild(columnTitle);
         nodeInfo.appendChild(destinationInfo)
 
         nextHopInfo = document.createElement('div');
         nextHopInfo.className = 'next-hop-info';
         columnTitle = document.createElement('p');
-        columnTitle.innerHTML = 'Next hop';
+        columnTitle.innerHTML = 'Next';
         nextHopInfo.appendChild(columnTitle);
         nodeInfo.appendChild(nextHopInfo);
 
         hopCountInfo = document.createElement('div');
         hopCountInfo.className = 'hop-count-info';
         columnTitle = document.createElement('p');
-        columnTitle.innerHTML = 'Hop count';
+        columnTitle.innerHTML = 'Metric';
         hopCountInfo.appendChild(columnTitle);
         nodeInfo.appendChild(hopCountInfo);
 
@@ -69,6 +69,19 @@ $(document).ready(function() {
         ownInfo = document.createElement('p');
         ownInfo.innerHTML = this.name;
         destinationInfo.appendChild(ownInfo);
+
+        ownInfo = document.createElement('p');
+        ownInfo.innerHTML = this.name;
+        nextHopInfo.appendChild(ownInfo);
+
+        ownInfo = document.createElement('p');
+        ownInfo.innerHTML = '0';
+        hopCountInfo.appendChild(ownInfo);
+
+        ownInfo = document.createElement('p');
+        ownInfo.innerHTML = this.name + '-0';
+        ownInfo.className = 'seq-no';
+        seqNoInfo.appendChild(ownInfo);
 
 		nodeInfoContainer.appendChild(nodeInfo);
 		$(this.blockInstance).append(nodeInfoContainer);
@@ -121,20 +134,63 @@ $(document).ready(function() {
                     var distance = Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
 
                     if (distance < 400) {
-                        var alreadyIn = false;
-                        for (var k = 0; k < nodes[j].table.length; k++) {
-                            if (nodes[j].table[k].destination == nodes[i].name) {
-                                alreadyIn = true;
+
+                        for (var k = 0; k < nodes[i].table.length; k++) {
+                            var alreadyIn = false;
+                            for (var l = 0; l < nodes[j].table.length; l++) {
+                                if (nodes[i].table[k].destination == nodes[j].table[l].destination) {
+                                    alreadyIn = true;
+                                }
+                                if (nodes[i].name == 1 && nodes[i].table[k].destination == 2 && nodes[j].name == 0) {
+                                    //alert(nodes[j].table[l].destination);
+                                }
                             }
-                        }
-                        if (alreadyIn) {
-                            
-                        } else {
-                            nodes[j].table.push({destination: nodes[i].name, nextHop: 0, hopCount: 0, seqNo: 0})
-                            var infoBlock = nodes[j].blockInstance.getElementsByClassName('destination-info');
-                            newInfo = document.createElement('p');
-                            newInfo.innerHTML = nodes[i].table[0].destination;
-                            infoBlock[0].appendChild(newInfo);
+
+
+
+                            if (alreadyIn) {
+
+                            } else {
+                                nodes[i].table[k].seqNo++;
+
+                                var destination = nodes[i].table[k].destination;
+                                var nextHop = nodes[i].name;
+                                var hopCount = nodes[i].table[k].hopCount + 1;
+                                var seqNo = nodes[i].table[k].seqNo;
+
+                                var column, newInfo;
+
+                                column = nodes[i].blockInstance.getElementsByClassName('seq-no');
+                                column[0].innerHTML = destination + '-' + seqNo;
+
+                                nodes[j].table.push({
+                                    destination: destination,
+                                    nextHop: nextHop,
+                                    hopCount: hopCount,
+                                    seqNo: seqNo
+                                })
+
+                                column = nodes[j].blockInstance.getElementsByClassName('destination-info');
+                                newInfo = document.createElement('p');
+                                newInfo.innerHTML = destination
+                                column[0].appendChild(newInfo);
+
+                                column = nodes[j].blockInstance.getElementsByClassName('next-hop-info');
+                                newInfo = document.createElement('p');
+                                newInfo.innerHTML = nextHop;
+                                column[0].appendChild(newInfo);
+
+                                column = nodes[j].blockInstance.getElementsByClassName('hop-count-info');
+                                newInfo = document.createElement('p');
+                                newInfo.innerHTML = hopCount;
+                                column[0].appendChild(newInfo);
+
+                                column = nodes[j].blockInstance.getElementsByClassName('seq-no-info');
+                                newInfo = document.createElement('p');
+                                newInfo.innerHTML = seqNo;
+                                newInfo.className = 'seq-no';
+                                column[0].appendChild(newInfo);
+                            }
                         }
                     }
                 }
